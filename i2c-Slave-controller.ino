7579944165c277b53b93a32878b538f10be611fb
4A LLC <midnight1979@mail.ru>
2016-04-14 09:14:09 +0500

    Изменения по передаче значений с термопары
    Добавлен еще один элемент массива для передачи целой и дробной части
    температуры.
    Переделана процедура опроса MAX6675

--- i2c-Slave-controller.ino
+++ i2c-Slave-controller.ino
@@ -24,13 +24,20 @@ Ultrasonic ultrasonic2(6, 7, 8700);
   0 - расстояние в см. на ultrasonic1
   1 - расстояние в см. на ultrasonic2
   2 - уровень воды для бака с горячей водой
-  3 - температура термопары
+  3 - температура термопары (целая часть)
+  4 - температура термопары (дробная часть)
 */
-uint8_t SlaveResult[4] = {0, 0, 0, 0};
+uint8_t SlaveResult[5] = {0, 0, 0, 0, 0};
 
 //int ultrasonic1_cm          = 0;          // Расстояние от ультразвукового датчика до поверхности воды в домашней емкости (в см.)
 //int ultrasonic2_cm          = 0;          // Расстояние от ультразвукового датчика до поверхности воды в уличной емкости (в см.)
 //int HotWater_Temp           = 0;          // Температура горячей воды (показания термопары)
+
+/* Переменная с типом float для получения температуры и переменные для целой и дробной частей температуры с MAX6675 */
+float max6675temp = 0;
+int dataA         = 0;
+int dataB         = 0;
+
 int HotWaterTankLevel       = 0;          // Уровень воды в % (долях) для бака с горячей водой
 
 void setup() {
@@ -70,7 +77,7 @@ void loop() {
 // отправка в i2c массива с результатами замеров датчиков
 void requestEvent()
 {
-  Wire.write(SlaveResult, 4);         // ответ на запрос от Master-контроллера
+  Wire.write(SlaveResult, 5);        // ответ на запрос от Master-контроллера
 }
 
 // Получение уровня домашней емкости в см.
@@ -89,7 +96,7 @@ void StreetTankLevelCheck()
 // 4 фиксированных положения "пусто = 0", "минимум = 1", "середина = 2", "максимум = 3"
 void HotWaterTankLevelCheck()
 {
-  HotWaterTankLevel = 1;  // пока нет готового датчика временно передаем значение 1
+  HotWaterTankLevel = 2;  // пока нет готового датчика временно передаем значение 2
   SlaveResult[2] = HotWaterTankLevel;
   //HotWaterTankLevel = 0;  //пусто
   //HotWaterTankLevel = 1;  //минимум
@@ -100,5 +107,22 @@ void HotWaterTankLevelCheck()
 /* Чтение показаний термопары */
 void HotWaterTempCheck()
 {
-  SlaveResult[3] = thermocouple.readCelsius();
+  /* Получаем значение с типом float и разбираем его на целую и дробную части для передачи Master-контроллеру через i2c */
+  max6675temp = thermocouple.readCelsius();
+
+  /* Целая часть значения температуры */
+  dataA = int(max6675temp);
+
+  /* Дробная часть значения температуры (до десятых долей) - условия в зависимости от знака */
+  if (max6675temp <= 0)
+  {
+    dataB = (int(max6675temp) - max6675temp) * 10;
+  }
+  else
+  {
+    dataB = (max6675temp - int(max6675temp)) * 10;
+  }
+
+  SlaveResult[3] = dataA;     // Целая часть значения температуры
+  SlaveResult[4] = dataB;     // Дробная часть значения температуры
 }
