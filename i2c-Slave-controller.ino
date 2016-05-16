8d6193b828e84235096e5ffe4841061f875599ca
4A LLC <midnight1979@mail.ru>
2016-05-17 00:26:52 +0500

    Множественные изменения - добавлена работа с термисторами на аналоговых входах

--- i2c-Slave-controller.ino
+++ i2c-Slave-controller.ino
@@ -9,21 +9,29 @@
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-/* Используем для питания пины Arduino */
+/* Используем для питания max6675 пины Arduino */
 #define vccPin 3
 #define gndPin 2
 
-/* Переменные для уровня воды в баке с горячей водой в бане */
+/* Переменные для уровня воды в баке с горячей водой в бане (используем A0 аналоговый вход) */
 #define analog0Pin 0
 int analog0      = 0;
 
-/* Переменные для датчика прозрачности в накопительных емкостях */
-#define analog1Pin 2
-int analog1         = 0;
-int analog1_temp    = 0;
-int analog1_percent = 0;
+/* Переменные для терморезистора в парилке (используем A1 аналоговый вход) */
+#define analog1Pin 1
+int termo_sensor1          = 0;      // Переменная для чтения текущих показаний датчика прозрачности
 
-const int averageFactor = 5;               // коэффициент сглаживания показаний (0 = не сглаживать) - чем выше, тем больше "инерционность" - это видно на индикаторах
+/* Переменные для датчика прозрачности в накопительных емкостях (используем A2 аналоговый вход) */
+#define analog2Pin 2
+int analog2         = 0;      // Переменная для чтения текущих показаний датчика прозрачности
+int analog2_temp    = 0;      // Временная переменная для усреднения показаний датчика
+int analog2_percent = 0;      // Процент прозрачности
+
+/* Переменные для терморезистора горячей воды в парилке (используем A3 аналоговый вход) */
+#define analog3Pin 3
+int termo_sensor2       = 0;      // Переменная для чтения текущих показаний датчика прозрачности
+
+const int averageFactor = 5;      // коэффициент сглаживания показаний (0 = не сглаживать) - чем выше, тем больше "инерционность" - это видно на индикаторах
 
 // Инициализация 1-го ультразвукового датчика (Trig, Echo)
 Ultrasonic ultrasonic1(8, 9, 8700);
@@ -38,20 +46,17 @@ Ultrasonic ultrasonic2(6, 7, 8700);
   2 - уровень воды для бака с горячей водой
   3 - температура термопары (целая часть)
   4 - температура термопары (дробная часть)
+  5 - процент прозрачности воды в накопительной емкости
+  6 - температура воздуха в парилке
 */
-uint8_t SlaveResult[6] = {0, 0, 0, 0, 0, 0};
-
-// 0 - Расстояние от ультразвукового датчика до поверхности воды в домашней емкости (в см.)
-// 1 - Расстояние от ультразвукового датчика до поверхности воды в уличной емкости (в см.)
-// 2 - Уровень в емкости с горячей водой
-// 3 - Температура горячей воды целые (показания термопары)
-// 4 - Температура горячей воды десятые (показания термопары)
-// 5 - Прозрачность воды в накопительной емкости
+uint8_t SlaveResult[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 /* Переменная с типом float для получения температуры и переменные для целой и дробной частей температуры с MAX6675 */
-float max6675temp = 0;
-int dataA         = 0;
-int dataB         = 0;
+float max6675temp = 0;        // Переменная для чтения текущий показаний термопары
+
+/* Две переменных для разделения float на целую и дробную части, т.к. массив, передаваемый на мастер не понимает float */
+int dataA         = 0;        // Переменная для хранения целой части от значения float-переменной
+int dataB         = 0;        // Переменная для хранения дробной части от значения float-переменной
 
 int HotWaterTankLevel       = 0;          // Уровень воды в % (долях) для бака с горячей водой
 
@@ -64,7 +69,7 @@ void setup() {
   pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
   /* Подождем стабилизации чипа MAX6675 */
   Serial.println("MAX6675 test...");
-  delay(500);
+  delay(1000);
   Serial.println("MAX6675 ready.");
 
   /* Инициализация Slave-контроллера на 8 адресе i2c */
@@ -83,11 +88,17 @@ void loop() {
   // Проверка уровня воды в емкости с горячей водой (4 положения уровня)
   HotWaterTankLevelCheck();
 
-  // Проверка температуры с термопары на MAX6675 (температура в баке с горячей водой)
-  HotWaterTempCheck();
-
   // Проверка прозрачности воды в накопительных емкостях
   WaterQuality_StreetTank();
+
+  // Проверка температуры воздуха в парилке
+  SaunaAirTermperature();
+
+  // Проверка температуры воды в парилке
+  SaunaWaterTermperature();
+
+  // Проверка температуры с термопары на MAX6675 (температура в баке с горячей водой)
+  SaunaStonesTempCheck();
 
 }
 
@@ -95,21 +106,23 @@ void loop() {
 // отправка в i2c массива с результатами замеров датчиков
 void requestEvent()
 {
-  Wire.write(SlaveResult, 6);        // ответ на запрос от Master-контроллера
+  Wire.write(SlaveResult, 8);        // ответ на запрос от Master-контроллера
 }
 
 // Получение уровня домашней емкости в см.
 void MainTankLevelCheck()
 {
   SlaveResult[0] = ultrasonic1.Ranging(CM);
-  //Serial.println(SlaveResult[0]);
+  //Serial.println(ultrasonic1.Ranging(CM));       // debug msg.
 }
 
 // Получение уровня уличной емкости в см.
 void StreetTankLevelCheck()
 {
   SlaveResult[1] = ultrasonic2.Ranging(CM);
-  //Serial.println(ultrasonic2.Ranging(CM));
+  //  Serial.println("US2:");
+  //  Serial.println(ultrasonic2.Ranging(CM));        // debug msg.
+
 }
 
 // Получение уровня бака с горячей водой в %
@@ -117,7 +130,6 @@ void StreetTankLevelCheck()
 void HotWaterTankLevelCheck()
 {
   analog0 = analogRead(analog0Pin);
-  //Serial.println(analog0);
   if (analog0 < 300)
   {
     HotWaterTankLevel = 3;  //пусто
@@ -138,16 +150,21 @@ void HotWaterTankLevelCheck()
     HotWaterTankLevel = 0;  //максимум
   }
 
-  SlaveResult[2] = HotWaterTankLevel;
+  SlaveResult[2] = HotWaterTankLevel;         // Пишем в 3-й элемент массива значение уровня емкости бака с горячей водой
 }
 
 /* Чтение показаний термопары */
-void HotWaterTempCheck()
+void SaunaStonesTempCheck()
 {
+  int air_stone_temp_diff = 0;
+
   /* Получаем значение с типом float и разбираем его на целую и дробную части для передачи Master-контроллеру через i2c */
   max6675temp = thermocouple.readCelsius();
-  delay(250);
+  delay(250);                                 // Задерка, как выяснилось, обязательна, иначе не обновляется значение переменной (работает от 150 но для большей уверенности поднял до 250)
   //Serial.println(max6675temp);
+
+  // Компенсируем показания термопары через температуру воздуха в парилке
+  air_stone_temp_diff = max6675temp - termo_sensor1;
 
   /* Целая часть значения температуры */
   dataA = int(max6675temp);
@@ -162,25 +179,50 @@ void HotWaterTempCheck()
     dataB = (max6675temp - int(max6675temp)) * 10;
   }
 
-  SlaveResult[3] = dataA;     // Целая часть значения температуры
-  SlaveResult[4] = dataB;     // Дробная часть значения температуры
+  SlaveResult[3] = dataA;             // Верхний байт значения температуры
+  SlaveResult[4] = (dataA >> 8);      // Нижний байт значения температуры
+
+  /* Отладочные сообщения */
+  //  int exper = (SlaveResult[4] << 8)|SlaveResult[3];
+  //  Serial.println(exper);
+  //  Serial.println(dataA);
 }
 
+/* Читаем показания датчика прозрачности в накопительных емкостях
+  датчик работает по принципу - чем выше прозрачность, тем большее выходное напряжение на входе (от 0V до 5V) */
 void WaterQuality_StreetTank()
 {
-  int oldsensorValue = analog1_temp;
+  int oldsensorValue = analog2_temp;
 
-  analog1 = analogRead(analog1Pin);
+  analog2 = analogRead(analog2Pin);
   //  Serial.print("RAW: ");
-  //  Serial.println(analog1);
+  //  Serial.println(analog2);
 
-  analog1_temp = (oldsensorValue * (averageFactor - 1) + analog1) / averageFactor;
-  analog1_percent = map(analog1_temp, 0, 1023, 0, 100) + (averageFactor - 1);
+  analog2_temp = (oldsensorValue * (averageFactor - 1) + analog2) / averageFactor;
+  analog2_percent = map(analog2_temp, 0, 1023, 0, 100) + (averageFactor - 1);
 
   //  Serial.print("average: ");
-  //  Serial.println(analog1_temp);
+  //  Serial.println(analog2_temp);
   //  Serial.print("%: ");
-  //  Serial.println(analog1_percent);
+  //  Serial.println(analog2_percent);
 
-  SlaveResult[5] = analog1_percent;   // Прозрачность воды в накопительных емкостях
+  SlaveResult[5] = analog2_percent;   // Прозрачность воды в накопительных емкостях пишем в 6-й элемент массива
+}
+
+void SaunaAirTermperature()
+{
+  termo_sensor1 = analogRead(analog1Pin);
+  termo_sensor1 = map(termo_sensor1, 282, 1023, 28, 150);
+  //Serial.println(termo_sensor1);
+
+  SlaveResult[6] = termo_sensor1;
+}
+
+void SaunaWaterTermperature()
+{
+  termo_sensor2 = analogRead(analog3Pin);
+  termo_sensor2 = map(termo_sensor2, 191, 1023, 28, 150);
+  //Serial.println(termo_sensor1);
+
+  SlaveResult[7] = termo_sensor2;
 }
